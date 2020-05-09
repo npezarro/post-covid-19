8baaf868d1b030f4c9f12614c6c7d883bd21bfff
Nicholas Pezarro <nicholaspezarro@Nicholass-MacBook-Pro.local>
2020-05-09 11:55:53 -0700

    Chapter 10 complete

--- microblog/config.py
+++ microblog/config.py
@@ -7,10 +7,15 @@ class Config(object):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    MAIL_SERVER = os.environ.get('MAIL_SERVER')
-    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
-    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    #MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    #MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    #MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    #MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    #MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = 1
+    MAIL_USERNAME = 'postcovid19project@gmail.com'
+    MAIL_PASSWORD = '4g!y5Hj%b'
     ADMINS = ['your-email@example.com']
     POSTS_PER_PAGE = 25
